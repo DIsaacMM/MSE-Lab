3fb83afe11fec987b22839f53acfe7fa331e2494
David <davidisaacmijares@gmail.com>
2026-05-06 21:12:44 -0700

    Modulo UART

---
+++ Lab4/sources/main.c
@@ -0,0 +1,19 @@
+﻿/**
+ * @file main.c
+ * @brief 
+ *
+ * @authors David Mijares
+ */
+
+
+
+int main(void)
+{ 
+
+    while (1)
+    {
+
+    }
+    
+    return 0;  // Never reached
+}
---
+++ Lab4/sources/serial.c
@@ -0,0 +1,14 @@
+
+
+
+
+void serial_init()
+{
+
+}
+
+void serial_printf()
+{
+    
+}
+
